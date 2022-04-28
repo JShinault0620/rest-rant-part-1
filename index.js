6a6da7c5183054f6071f19502b6f3faca4530e70
JShinault0620 <jshincool@gmail.com>
2022-04-27 21:20:45 -0400

    Added 404 error page

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, req) => {
+require('dotenv').config()
+
+app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>The page you are looking for does not exist.</h1>')
+})
+
+app.listen(process.env.PORT)
