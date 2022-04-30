7d2e11c8ad76e3f5206695a6aef8a93d1ab992a1
JShinault0620 <jshincool@gmail.com>
2022-04-30 13:15:12 -0400

    Added endpoints to README as well as required data.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 This is a mock site to display restaurant reviews.
+
+
+Availables paths include:
+
+GET -> /
+GET/POST -> /places
+GET -> /places/new
+GET/PUT/DELETE -> /places:id
+GET -> /places/:id/edit
+POST -> /places/:id/rant
+DELETE -> /places/:id/rant/:randId
+
+
+Your places should have:
+
+name (string)
+city (string)
+state (string)
+cuisines (string)
+pic (string)
--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,40 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+router.post('/', (req, res) => {
+    res.send('POST /places')
+})
+
+router.get('/new', (req, res) => {
+    res.send('GET /places/new')
+})
+
+router.get('/:id', (req, res) => {
+    res.send(`GET /places/${req.params.id}`)
+})
+
+router.put('/:id', (req, res) => {
+    res.send(`PUT /places/${req.params.id}`)
+})
+
+router.get('/:id/edit', (req, res) => {
+    res.send(`PUT /places/${req.params.id}/edit`)
+})
+
+router.delete('/:id', (req, res) => {
+    res.send(`DELETE /places/${req.params.id}`)
+})
+
+router.post('/:id/rant', (req, res) => {
+    res.send(`POST /places/${req.params.id}/rant`)
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send(`DELETE /places/${req.params.id}/rant/${req.params.rantId}`)
+})
+
+router.get('*', (req, res) => {
+    res.status(404).send('<h1>The page you are looking for does not exist.</h1>')
+})
+
 module.exports = router
