0c6e1230f2626fab9ce995fff0375f5130cd2516
JShinault0620 <jshincool@gmail.com>
2022-05-04 17:45:02 -0400

    Passing form data

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 // Routes
 app.use('/places', require('./controllers/places'))
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.status(404).render('error404')
 })
+
 
 // Server listening
 app.listen(process.env.PORT, () => {
