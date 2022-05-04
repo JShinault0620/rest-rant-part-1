5794c31d5818cf0efaa6f5a8a10283810b02b814
JShinault0620 <jshincool@gmail.com>
2022-05-04 18:36:22 -0400

    Navbar and style

--- views/default.jsx
+++ views/default.jsx
@@ -11,6 +11,19 @@ function Def (html) {
                 <link rel="stylesheet" href="./main.css"></link>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
